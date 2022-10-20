40811fbe4dfde4867fc6bd2eba21749499a69dce
sanjayym <sanjayspear@gmail.com>
2022-10-20 11:34:13 +0530

    Commit my new changes

---
+++ src/main/java/org/launchCode/javabasics/hello.java
@@ -0,0 +1,9 @@
+package org.launchCode.javabasics;
+
+public class hello {
+    @Override
+    public String toString() {
+        System.out.println("hiskjhas");
+        return super.toString();
+    }
+}
